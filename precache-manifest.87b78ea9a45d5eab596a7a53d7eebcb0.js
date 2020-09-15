fc6e0c38f6a26f5986dc48ddda9176507be05a96
unknown <sunil.raipur@gmail.com>
2020-09-15 13:35:17 +0530

    amazon clone

---
+++ precache-manifest.87b78ea9a45d5eab596a7a53d7eebcb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27658101effe3cf49dafda8424a9c1ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0341dbd2c1970fffb858",
+    "url": "/static/css/main.031f2302.chunk.css"
+  },
+  {
+    "revision": "50eb1631219ba2645627",
+    "url": "/static/js/2.a886b0b2.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.a886b0b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0341dbd2c1970fffb858",
+    "url": "/static/js/main.b7bbad6e.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
